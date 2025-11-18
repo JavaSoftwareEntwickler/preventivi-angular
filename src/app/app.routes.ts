5d05758509230fadde7ef782e2754c09566651c8
JavaSoftwareEntwickler <mm.java.software@gmail.com>
2025-11-18 19:34:10 +0100

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './dashboard/dashboard';
+import { Preventivo } from './preventivo/preventivo';
+import { Settings } from './settings/settings';
+import { Pages } from './pages/pages';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: Dashboard },
+  { path: 'preventivo', component: Preventivo},
+  { path: 'settings', component: Settings },
+  { path: 'pages', component: Pages },
+];
---
+++ src/app/preventivo/preventivo.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-preventivo',
+  imports: [],
+  templateUrl: './preventivo.html',
+  styleUrl: './preventivo.css',
+})
+export class Preventivo {
+
+}
