35e51e4e5c6cc8b615b888ddd0dbaaff09a0c7f6
JavaSoftwareEntwickler <mm.java.software@gmail.com>
2025-11-21 23:48:06 +0100

    code format

--- src/app/preventivo/components/preventivo-actions/actions.ts
+++ src/app/preventivo/components/preventivo-actions/actions.ts
@@ -2,16 +2,16 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 
 @Component({
-selector: 'app-preventivo-actions',
-standalone: true,
-templateUrl: './actions.html',
-styleUrls: ['./actions.css']
+    selector: 'app-preventivo-actions',
+    standalone: true,
+    templateUrl: './actions.html',
+    styleUrls: ['./actions.css']
 })
 export class PreventivoActions {
-@Input() isEditing = false;
-@Output() edit = new EventEmitter<void>();
-@Output() save = new EventEmitter<void>();
-@Output() cancel = new EventEmitter<void>();
-@Output() print = new EventEmitter<void>();
-@Output() delete = new EventEmitter<void>();
+    @Input() isEditing = false;
+    @Output() edit = new EventEmitter<void>();
+    @Output() save = new EventEmitter<void>();
+    @Output() cancel = new EventEmitter<void>();
+    @Output() print = new EventEmitter<void>();
+    @Output() delete = new EventEmitter<void>();
 }
