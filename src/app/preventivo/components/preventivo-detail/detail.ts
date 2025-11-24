a957ec792a28045c585ff95e0e63544f9c09cce8
JavaSoftwareEntwickler <mm.java.software@gmail.com>
2025-11-24 15:22:05 +0100

    pulizia linee di dubug

--- src/app/preventivo/components/preventivo-detail/detail.ts
+++ src/app/preventivo/components/preventivo-detail/detail.ts
@@ -25,17 +25,9 @@ export class PreventivoDetail {
 
 
     startEdit() { this.svc.editPreventivo(); }
-    save() {
-        console.log('salvo');
-        this.svc.savePreventivo(); this.back.emit();
-    }
+    save() { this.svc.savePreventivo(); this.back.emit(); }
     cancel() { this.svc.cancelEdit(); this.back.emit(); }
-    doPrint() {
-        console.log('stampooooooooooooo');
-        if (this.preventivo) this.svc.printPreventivo(this.preventivo);
-    }
+    doPrint() { if (this.preventivo) this.svc.printPreventivo(this.preventivo); }
     doDelete() { if (this.preventivo) this.svc.deletePreventivo(this.preventivo); this.back.emit(); }
-    doAddRow() {
-        console.log('Aggiungo riga');
-    }
+    doAddRow() { console.log('Aggiungo riga'); }
 }
