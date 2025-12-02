ad02daf673000bade60ea00def6af293b4279646
JavaSoftwareEntwickler <mm.java.software@gmail.com>
2025-12-02 11:11:19 +0100

    feat (elimina riga preventivo) Aggiunta l'icona del cestino su ogni riga del preventivo che è vibile sul form solo se si è in modalità modifica o aggiunta preventivo. L'aggiunta è solo Lato FE manca la logica di businesse. Sarà utile per cancellare la riga del preventivo in maniera molto intuittiva sia in fase di modifica del preventivo che in fase di un nuovo preventivo.

--- src/app/preventivo/components/preventivo-form/form.ts
+++ src/app/preventivo/components/preventivo-form/form.ts
@@ -1,7 +1,6 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { PreventiviService } from '../../services/preventivi.service';
 import { PreventivoFormAdapter } from './PreventivoFormAdapter';
 
 
@@ -15,6 +14,7 @@ import { PreventivoFormAdapter } from './PreventivoFormAdapter';
 export class PreventivoForm implements OnChanges {
     @Input() preventivo: any | null = null;
     @Input() formPreventivo!: FormGroup; // <- passiamo il form dal padre
+    @Input() isEditing = false;
     constructor(public frmAdapter: PreventivoFormAdapter) { }
 
 
