89f113e5a914694169ccd5ae0cb3c48a281139a0
JavaSoftwareEntwickler <mm.java.software@gmail.com>
2025-12-20 08:27:19 +0100

    *feat(sidebar): modifica voci e icone nel menu di navigazione*
    Modificato le etichette e le icone delle voci nel menu della sidebar:
    
    "Prodotti" ora ha l'icona fal fa-box-open.
    
    "Preventivi" ora ha l'icona fal fa-file.
    
    Refactoring della classe LeftSidebar:
    
    Ordinamento delle variabili e funzioni per migliorare la leggibilità e la manutenibilità del codice.
    
    Nessuna modifica nel comportamento della funzionalità della sidebar.

--- src/app/left-sidebar/left-sidebar.ts
+++ src/app/left-sidebar/left-sidebar.ts
@@ -4,43 +4,43 @@ import { RouterModule } from '@angular/router';
 import { CollapseOnNavigateDirective } from './collapse-on-navigate.directive';
 
 @Component({
-  selector: 'app-left-sidebar',
-  standalone: true,
-  imports: [RouterModule, CommonModule, CollapseOnNavigateDirective],
-  templateUrl: './left-sidebar.html',
-  styleUrl: './left-sidebar.css',
+    selector: 'app-left-sidebar',
+    standalone: true,
+    imports: [RouterModule, CommonModule, CollapseOnNavigateDirective],
+    templateUrl: './left-sidebar.html',
+    styleUrl: './left-sidebar.css',
 })
 export class LeftSidebar {
-  isLeftSidebarCollapsed = input.required<boolean>();
-  changeIsLeftSidebarCollapsed = output<boolean>();
-  items = [
-    {
-      routeLink: 'dashboard',
-      icon: 'fal fa-home',
-      label: 'Dashboard',
-    },
-    {
-      routeLink: 'preventivo',
-      icon: 'fal fa-box-open',
-      label: 'Preventivi',
-    },
-    {
-      routeLink: 'pages',
-      icon: 'fal fa-file',
-      label: 'Pages',
-    },
-    {
-      routeLink: 'settings',
-      icon: 'fal fa-cog',
-      label: 'Settings',
-    },
-  ];
+    isLeftSidebarCollapsed = input.required<boolean>();
+    changeIsLeftSidebarCollapsed = output<boolean>();
+    items = [
+        {
+            routeLink: 'dashboard',
+            icon: 'fal fa-home',
+            label: 'Dashboard',
+        },
+        {
+            routeLink: 'preventivo',
+            icon: 'fal fa-file',
+            label: 'Preventivi',
+        },
+        {
+            routeLink: 'pages',
+            icon: 'fal fa-box-open',
+            label: 'Prodotti',
+        },
+        {
+            routeLink: 'settings',
+            icon: 'fal fa-cog',
+            label: 'Settings',
+        },
+    ];
 
-  toggleCollapse(): void {
-    this.changeIsLeftSidebarCollapsed.emit(!this.isLeftSidebarCollapsed());
-  }
+    toggleCollapse(): void {
+        this.changeIsLeftSidebarCollapsed.emit(!this.isLeftSidebarCollapsed());
+    }
 
-  closeSidenav(): void {
-    this.changeIsLeftSidebarCollapsed.emit(true);
-  }
+    closeSidenav(): void {
+        this.changeIsLeftSidebarCollapsed.emit(true);
+    }
 }
